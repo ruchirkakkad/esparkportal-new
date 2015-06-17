a7946b9452e3f115dd643350398eb08f0c0ed53a
Ruchir <ruchir.kakkad@gmail.com>
2015-06-17 14:01:56 +0530

    17jun2015 file type conversion testing

--- app/routes.php
+++ app/routes.php
@@ -91,9 +91,9 @@ Route::get('migrate',function(){
     echo '<br>done migrate:install';
 });
 Route::get('fileconvert',function(){
-    CloudConvert::file('/uploads/admin@admin.com/attachments/initial questions of rippled waters.docx')->to('/uploads/admin@admin.com/attachments/t.jpg');
+//    CloudConvert::file('/uploads/admin@admin.com/attachments/initial questions of rippled waters.docx')->to('/uploads/admin@admin.com/attachments/t.jpg');
 
-    CloudConvert::file('/uploads/admin@admin.com/attachments/Lighthouse.jpg')->quality(70)->to('png');
+    CloudConvert::file('/uploads/admin@admin.com/attachments/Lighthouse.jpg')->to('png');
 });
 Route::get('screenshots',function(){
     CloudConvert::website('www.google.com')->to('google.jpg');
