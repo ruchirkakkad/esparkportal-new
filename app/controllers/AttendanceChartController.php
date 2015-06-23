fe8b2129f72e7b292796b333d1faf07110253b26
Ruchir <ruchir.kakkad@gmail.com>
2015-06-23 18:06:56 +0530

    23jun2015 attendance_chart added leaves and holidays..

--- app/controllers/AttendanceChartController.php
+++ app/controllers/AttendanceChartController.php
@@ -19,31 +19,59 @@ class AttendanceChartController extends \BaseController
         $last_day_this_month = date("Y-m-d", strtotime($last_day_this_month . ' +1 day'));
         $returndata = [];
         $dateCountArr = [];
+        $holidays = Holiday::lists('holidays_name', 'holidays_date');
+
         foreach ($users as $k => $v) {
             $returndata[$k]['user_id'] = $v->user_id;
             $returndata[$k]['first_name'] = $v->first_name;
             $returndata[$k]['last_name'] = $v->last_name;
             $count = 0;
             $date = $first_day_this_month;
+
+            $leaves_of_month = Leave::join('leave_types', 'leaves.leave_types_id', '=', 'leave_types.leave_types_id')
+                ->where('users_id', '=', $v->user_id)
+                ->whereRaw("MONTH(leave_date) = $month")
+                ->whereRaw("YEAR(leave_date) = $year")
+                ->where("leave_status", "=", "final-approve")
+                ->lists('leave_title', 'leave_date');
             while ($date != $last_day_this_month) {
+                $day = date('D', strtotime($date));
                 $today = date("Y-m-d");
                 $dateNumber = date("d", strtotime($date));
+                $returndata[$k]['dates'][$dateNumber]['day'] = $day;
+                if (array_key_exists($date, $holidays)) {
+                    $returndata[$k]['dates'][$dateNumber]['flag'] = 'H';
+                    $next_date = date('Y-m-d', strtotime($date . ' +1 day'));
+                    $date = $next_date;
+                    continue;
+                }
                 if (!isset($dateCountArr[$dateNumber])) {
                     $dateCountArr[$dateNumber] = 0;
                 }
                 if (strtotime($date) <= strtotime($today)) {
-                    $data1 = Staffing::where('users_id', '=', $v->user_id)
-                        ->whereRaw("DATE_FORMAT(check_in,'%Y-%m-%d') = '$date'")->get();
-
-                    if (count($data1) == 1) {
-                        $returndata[$k]['dates'][$dateNumber]['flag'] = 'P';
-                        $count++;
-                        $dateCountArr[$dateNumber]++;
+                    if (array_key_exists($date, $leaves_of_month)) {
+                        $returndata[$k]['dates'][$dateNumber]['flag'] = $leaves_of_month[$date];
                     } else {
-                        $returndata[$k]['dates'][$dateNumber]['flag'] = 'A';
+                        $data1 = Staffing::where('users_id', '=', $v->user_id)
+                            ->whereRaw("DATE_FORMAT(check_in,'%Y-%m-%d') = '$date'")->get();
+                        if (count($data1) == 1) {
+                            $returndata[$k]['dates'][$dateNumber]['flag'] = 'P';
+                            $count++;
+                            $dateCountArr[$dateNumber]++;
+                        } else {
+                            if ($day == 'Sat' || $day == 'Sun') {
+                                $returndata[$k]['dates'][$dateNumber]['flag'] = 'H';
+                            } else {
+                                $returndata[$k]['dates'][$dateNumber]['flag'] = 'A';
+                            }
+                        }
                     }
                 } else {
-                    $returndata[$k]['dates'][$dateNumber]['flag'] = '';
+                    if ($day == 'Sat' || $day == 'Sun') {
+                        $returndata[$k]['dates'][$dateNumber]['flag'] = 'H';
+                    } else {
+                        $returndata[$k]['dates'][$dateNumber]['flag'] = '';
+                    }
                 }
                 $next_date = date('Y-m-d', strtotime($date . ' +1 day'));
                 $date = $next_date;
